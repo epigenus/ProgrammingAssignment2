499b0bcbdd21fce077272056c39338e1906d4506
epigenus <epigenus@gmail.com>
2014-07-25 19:36:33 -0700

    Initial Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two R functions for solving the inverse of a (potentially large)
+## matrix by caching results as they are computed.
+## 
+## Function makeCache Matrix ~~~
+## 
+## Function cacheSolve ~~~
+##
+# Submitted in partial completion of 'Programming Assignment 2" for 
+# course R Programming as part of the Data Science Track on Coursera
+# Submitted by: epigenus
+# Section: rprog-005
+# Date: August 2014
 
-## Write a short comment describing this function
+
+## Function: makeCacheMatrix(x)
+## makeCacheMatrix takes as input a matrix, 'x', and output a set of functions
+## for caching the results of the computation of the inverse of the matrix.
+## By default the function will create an empty matrix when no other input is given.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Function: cacheSolve(x, ...)
+## cacheSolve takes as input a matrix, 'x', with other arguments and outputs the
+## inverse of the matrix 'x'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
